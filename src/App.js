b18113828d4c4e41449942c0c6e8ac2620ec39a6
yoanan1996 <jvasquezl17@miumg.edu.gt>
2024-10-06 23:23:01 -0600

    Sabado cinco oct

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App"> <h1 style={{ color: 'blue', fontSize: '4rem' }}>¡Hola Mundo en React!</h1> </div>
+
+  );
+}
+
+export default App;
